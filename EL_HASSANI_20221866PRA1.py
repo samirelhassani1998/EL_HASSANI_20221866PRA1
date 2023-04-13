0fd7cc2f9a7ba904bd1e897d2fd1eefe29ae603a
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-04-13 12:28:34 +0200

    Update EL_HASSANI_20221866PRA1.py

--- EL_HASSANI_20221866PRA1.py
+++ EL_HASSANI_20221866PRA1.py
@@ -1,8 +1,11 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import gdown
+
+st.title("Analyse des données Uber - Avril 2014")
 
 # Télécharger le fichier CSV à partir de Google Drive
 url = "https://drive.google.com/uc?id=1qoK_zLLWWPjY3HaPN4WXH2q1OayJiQtz"
@@ -28,16 +31,28 @@ df['weekday'] = df['Date/Time'].map(get_weekday)
 df['hour'] = df['Date/Time'].map(get_hour)
 
 # Afficher les premières lignes, les dernières lignes et les informations du DataFrame
-print(df.head())
-print(df.tail())
-print(df.shape[0])
-print(df.describe())
-print(df.info())
+st.subheader("Premières lignes")
+st.write(df.head())
+
+st.subheader("Dernières lignes")
+st.write(df.tail())
+
+st.subheader("Nombre de lignes")
+st.write(df.shape[0])
+
+st.subheader("Statistiques descriptives")
+st.write(df.describe())
+
+st.subheader("Informations sur le DataFrame")
+st.write(df.info())
 
 # Créer un histogramme de la distribution du jour du mois
 sns.set(rc={'figure.figsize':(11.7,8.27)})
-sns.histplot(df['dom'], bins=30, kde=False)
-plt.title('Day of the Month Distribution - Uber - April 2014')
-plt.xlabel('Day of the Month')
-plt.ylabel('Frequency')
-plt.show()
+fig, ax = plt.subplots()
+sns.histplot(df['dom'], bins=30, kde=False, ax=ax)
+ax.set_title('Day of the Month Distribution - Uber - April 2014')
+ax.set_xlabel('Day of the Month')
+ax.set_ylabel('Frequency')
+
+st.subheader("Histogramme de la distribution du jour du mois")
+st.pyplot(fig)

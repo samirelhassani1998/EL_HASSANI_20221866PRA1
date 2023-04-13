2873a8e17608256e16e75f12bb5ae6713662a7d7
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-04-13 12:13:52 +0200

    Update EL_HASSANI_20221866PRA1.py

--- EL_HASSANI_20221866PRA1.py
+++ EL_HASSANI_20221866PRA1.py
@@ -1,1 +1,41 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+def main():
+    st.title("Day of the Month Distribution - Uber - April 2014")
+
+    df = pd.read_csv('/home/samir/Downloads/uber-raw-data-apr14.csv', delimiter=',')
+    df['Date/Time'] = pd.to_datetime(df['Date/Time'])
+
+    def get_dom(dt):
+        return dt.day
+
+    df['dom'] = df['Date/Time'].map(get_dom)
+
+    sns.set(rc={'figure.figsize':(11.7,8.27)})
+    fig = sns.histplot(df['dom'], bins=30, kde=False).get_figure()
+    plt.title('Day of the Month Distribution - Uber - April 2014')
+    plt.xlabel('Day of the Month')
+    plt.ylabel('Frequency')
+    
+    st.pyplot(fig)
+
+    def get_weekday(dt):
+        return dt.weekday()
+
+    df['weekday'] = df['Date/Time'].map(get_weekday)
+
+    def get_hour(dt):
+        return dt.hour
+
+    df['hour'] = df['Date/Time'].map(get_hour)
+
+    st.write(df.head())
+    st.write(df.describe())
+    st.write(df.info())
+
+if __name__ == "__main__":
+    main()

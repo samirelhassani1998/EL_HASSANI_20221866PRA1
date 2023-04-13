f6f84dd9199160bde18b58107c9e38ace6a87c7a
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-04-13 15:20:28 +0200

    Update EL_HASSANI_20221866PRA1.py

--- EL_HASSANI_20221866PRA1.py
+++ EL_HASSANI_20221866PRA1.py
@@ -1,153 +1,88 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-import gdown
-st.title("Analyse des données Uber - Avril 2014")
-
-# Télécharger le fichier CSV à partir de Google Drive
-url = "https://drive.google.com/uc?id=1qoK_zLLWWPjY3HaPN4WXH2q1OayJiQtz"
-gdown.download(url, "uber-raw-data-apr14.csv", quiet=False)
-
-# Charger les données
-df = pd.read_csv("uber-raw-data-apr14.csv", delimiter=',')
-df['Date/Time'] = pd.to_datetime(df['Date/Time'])
-
-# Fonctions pour extraire les informations pertinentes
-def get_dom(dt):
-    return dt.day
-
-def get_weekday(dt):
-    return dt.weekday()
-
-def get_hour(dt):
-    return dt.hour
-
-# Appliquer les fonctions pour extraire les informations pertinentes
-df['dom'] = df['Date/Time'].map(get_dom)
-df['weekday'] = df['Date/Time'].map(get_weekday)
-df['hour'] = df['Date/Time'].map(get_hour)
-
-# Afficher les premières lignes, les dernières lignes et les informations du DataFrame
-st.subheader("Premières lignes")
-st.write(df.head())
-
-st.subheader("Dernières lignes")
-st.write(df.tail())
-
-st.subheader("Nombre de lignes")
-st.write(df.shape[0])
-
-st.subheader("Statistiques descriptives")
-st.write(df.describe())
-
-st.subheader("Informations sur le DataFrame")
-st.write(df.info())
-
-# Créer un histogramme de la distribution du jour du mois
-sns.set(rc={'figure.figsize':(11.7,8.27)})
-fig, ax = plt.subplots()
-sns.histplot(df['dom'], bins=30, kde=False, ax=ax)
-ax.set_title('Day of the Month Distribution - Uber - April 2014')
-ax.set_xlabel('Day of the Month')
-ax.set_ylabel('Frequency')
-
-st.subheader("Histogramme de la distribution du jour du mois")
-st.pyplot(fig)
-
+import matplotlib.pyplot as plt
 import pydeck as pdk
+import gdown
+import os
 
-st.title("Carte GeoJSON avec PyDeck et Streamlit")
-
-# Définir l'URL de la source de données
-DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json"
-
-# Définir les coordonnées du polygone de couverture de terrain
-LAND_COVER = [[[-123.0, 49.196], [-123.0, 49.324], [-123.306, 49.324], [-123.306, 49.196]]]
-
-# Définir l'état initial de la vue
-INITIAL_VIEW_STATE = pdk.ViewState(latitude=49.254, longitude=-123.13, zoom=11, max_zoom=16, pitch=45, bearing=0)
-
-# Créer une couche pour le polygone de couverture de terrain
-polygon = pdk.Layer(
-    "PolygonLayer",
-    LAND_COVER,
-    stroked=False,
-    # traite les données comme une paire de longitude-latitude plate
-    get_polygon="-",
-    get_fill_color=[0, 0, 0, 20],
-)
-
-# Créer une couche pour les données géospatiales
-geojson = pdk.Layer(
-    "GeoJsonLayer",
-    DATA_URL,
-    opacity=0.8,
-    stroked=False,
-    filled=True,
-    extruded=True,
-    wireframe=True,
-    get_elevation="properties.valuePerSqm / 20",
-    get_fill_color="[255, 255, properties.growth * 255]",
-    get_line_color=[255, 255, 255],
-)
-
-# Créer une carte contenant les deux couches
-r = pdk.Deck(layers=[polygon, geojson], initial_view_state=INITIAL_VIEW_STATE)
-
-# Afficher la carte dans Streamlit
-st.pydeck_chart(r)
-
+# Titre de l'application Streamlit
 st.title("Analyse des trajets de taxi à New York")
 
-st.header("Données")
+# Fonction pour charger les données
+@st.cache
+def load_data():
+    path = "https://raw.githubusercontent.com/uber-web/kepler.gl-data/master/nyctrips/data.csv"
+    df = pd.read_csv(path, delimiter=',')
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    return df
 
-path2 = "https://raw.githubusercontent.com/uber-web/kepler.gl-data/master/nyctrips/data.csv"
-df3 = pd.read_csv(path2, delimiter=',')
+# Chargement des données
+df = load_data()
 
-st.write(df3.head())
-
+# Section Informations générales sur le DataFrame
 st.header("Informations générales sur le DataFrame")
-st.write(df3.info())
+st.write(df.info())
 
+# Section Statistiques descriptives pour chaque colonne
 st.header("Statistiques descriptives pour chaque colonne")
-st.write(df3.describe())
+st.write(df.describe())
 
-st.header("Analyses")
+# Section Analyses
 
-mean_trip_distance = df3['trip_distance'].mean()
+# Moyenne de la distance parcourue par trajet
+mean_trip_distance = df['trip_distance'].mean()
+
+# Moyenne du montant des pourboires
+mean_tip_amount = df['tip_amount'].mean()
+
+# Moyenne du nombre de passagers par trajet
+mean_passenger_count = df['passenger_count'].mean()
+
+# Moyenne du montant total des trajets
+mean_total_amount = df['total_amount'].mean()
+
+# Nombre de trajets par nombre de passagers
+passenger_count_counts = df['passenger_count'].value_counts()
+
+# Section Affichage des résultats des analyses
+st.header("Résultats des analyses")
+
+# Affichage de la distance moyenne parcourue par trajet
 st.write(f"Distance moyenne parcourue par trajet : {mean_trip_distance:.2f} km")
 
-mean_tip_amount = df3['tip_amount'].mean()
+# Affichage du montant moyen des pourboires
 st.write(f"Montant moyen des pourboires : {mean_tip_amount:.2f} $")
 
-mean_passenger_count = df3['passenger_count'].mean()
+# Affichage de la moyenne du nombre de passagers par trajet
 st.write(f"Moyenne du nombre de passagers par trajet : {mean_passenger_count:.2f}")
 
-mean_total_amount = df3['total_amount'].mean()
+# Affichage du montant moyen total des trajets
 st.write(f"Montant moyen total des trajets : {mean_total_amount:.2f} $")
 
-passenger_count_counts = df3['passenger_count'].value_counts()
+# Affichage du nombre de trajets par nombre de passagers
 st.write("Nombre de trajets par nombre de passagers :")
 st.write(passenger_count_counts)
 
-st.header("Visualisations")
+# Section Visualisations
 
+# Histogramme des distances de trajet
 st.subheader("Histogramme des distances de trajet")
+bins = st.slider('Nombre de barres', 1, 100, 50)
 plt.figure()
-sns.histplot(df3['trip_distance'], bins=50)
+sns.histplot(df['trip_distance'], bins=bins)
 plt.title("Histogramme des distances de trajet")
 plt.xlabel("Distance (km)")
 plt.ylabel("Nombre de trajets")
 st.pyplot(plt)
 
+# Boxplot des montants totaux des trajets
 st.subheader("Boxplot des montants totaux des trajets")
 plt.figure()
-sns.boxplot(x=df3['total_amount'])
+sns.boxplot(x=df['total_amount'])
 plt.title("Boxplot des montants totaux des trajets")
 plt.xlabel("Montant total ($)")
-st.pyplot(plt)
+
 
 st.subheader("Nombre de trajets par nombre de passagers")
 plt.figure()
